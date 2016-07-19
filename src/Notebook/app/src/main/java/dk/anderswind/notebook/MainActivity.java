ee770c5ede17e5aaa19c5fbbcbac407dda5e34cc
Anders Wind <awia00@gmail.com>
2016-07-20 00:34:02 +0200

    Preferences

--- src/Notebook/app/src/main/java/dk/anderswind/notebook/MainActivity.java
+++ src/Notebook/app/src/main/java/dk/anderswind/notebook/MainActivity.java
@@ -1,7 +1,10 @@
 package dk.anderswind.notebook;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        loadPreferences();
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -53,6 +58,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, AppPreferences.class);
+            startActivity(intent);
             return true;
         } else if (id == R.id.action_add_note)
         {
@@ -64,5 +71,18 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void loadPreferences()
+    {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isBackgroundDark = sharedPreferences.getBoolean("background_color", false);
+        if (isBackgroundDark)
+        {
+            LinearLayout mainLayout = (LinearLayout) findViewById(R.id.mainActivityLayout);
+            mainLayout.setBackgroundColor(Color.parseColor("#3c3f41"));
+        }
+        String notebookTitle = sharedPreferences.getString("title", "Notebook");
+        setTitle(notebookTitle);
     }
 }
